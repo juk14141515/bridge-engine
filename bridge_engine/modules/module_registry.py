ed88e92df8d4f0512b4f04ba9430682b478734fe
juk14141515 <zjuliosteele@icloud.com>
2026-05-09 00:40:23 -0700

    Upgrade registry to weighted module resolution

--- bridge_engine/modules/module_registry.py
+++ bridge_engine/modules/module_registry.py
@@ -10,8 +10,35 @@ MODULES = [
 
 class ModuleRegistry:
     def resolve(self, interest: str, learning_goal: str):
-        for module in MODULES:
-            if module.supports(interest, learning_goal):
-                return module
+        weighted_modules = []
 
-        return CodingExecutionModule()
+        for module in MODULES:
+            score = 0
+
+            if module.supports(interest, learning_goal):
+                score += 1
+
+            if module.supports(interest, interest):
+                score += 1
+
+            if module.supports(learning_goal, learning_goal):
+                score += 2
+
+            if score > 0:
+                weighted_modules.append({
+                    "module": module,
+                    "weight": score,
+                })
+
+        if not weighted_modules:
+            weighted_modules.append({
+                "module": CodingExecutionModule(),
+                "weight": 1,
+            })
+
+        weighted_modules.sort(
+            key=lambda item: item["weight"],
+            reverse=True,
+        )
+
+        return weighted_modules
