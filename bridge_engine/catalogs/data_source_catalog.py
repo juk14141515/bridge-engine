4beacb5f9a2363d8b42d073184c1b28301b2d919
juk14141515 <zjuliosteele@icloud.com>
2026-05-09 01:20:26 -0700

    Add data source catalog

---
+++ bridge_engine/catalogs/data_source_catalog.py
@@ -0,0 +1,29 @@
+DATA_SOURCE_CATALOG = {
+    "lms_integrations": [
+        "canvas",
+        "blackboard",
+        "google_classroom",
+    ],
+    "document_sources": [
+        "google_docs",
+        "notion",
+        "pdf_uploads",
+        "screenshots",
+    ],
+    "development_sources": [
+        "github_repositories",
+        "local_projects",
+        "coding_sandboxes",
+    ],
+    "interaction_sources": [
+        "voice_recordings",
+        "clipboard_ingestion",
+        "browser_extension",
+        "study_analytics",
+    ],
+    "future_sources": [
+        "wearable_signals",
+        "focus_tracking",
+        "behavior_patterns",
+    ],
+}
