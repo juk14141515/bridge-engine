96e14e8ffb28500beeb5b9a41d6a416d7df98ae3
juk14141515 <zjuliosteele@icloud.com>
2026-05-10 21:42:27 -0700

    Add API runtime contract

---
+++ bridge_core/api_runtime_contract.py
@@ -0,0 +1,18 @@
+API_RUNTIME_CONTRACT = {
+    'session_create': {
+        'route': '/api/session/create',
+        'method': 'POST'
+    },
+    'session_continue': {
+        'route': '/api/session/continue',
+        'method': 'POST'
+    },
+    'session_rewrite': {
+        'route': '/api/session/rewrite',
+        'method': 'POST'
+    },
+    'workspace_load': {
+        'route': '/api/workspace/<id>',
+        'method': 'GET'
+    }
+}
